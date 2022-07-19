73d4181363adcbe74fe953c56ca7d40e6c9de619
ItsAtulMore <atul.more@ikaitechnologies.com>
2022-07-19 18:28:29 +0530

    saurav

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import img from "../images/08_CONTACTUS/map@2x.png";
 import { faPhone } from "@fortawesome/free-solid-svg-icons";
 
 const ContactUs = () => {

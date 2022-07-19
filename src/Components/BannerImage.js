5068f072bb289a285919aa6120266c69a24b31f2
ItsAtulMore <atul.more@ikaitechnologies.com>
2022-07-19 18:48:29 +0530

    saurav

--- src/Components/BannerImage.js
+++ src/Components/BannerImage.js
@@ -1,6 +1,6 @@
 import { graphql, useStaticQuery } from "gatsby";
 import BackgroundImage from "gatsby-background-image";
-import video1 from "../images/ImmersionxShowreel_v02.mp4";
+import video1 from "../images/Untitled.mp4";
 import React from "react";
 import { Fade } from "react-reveal";
 
